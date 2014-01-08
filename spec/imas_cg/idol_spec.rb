8a8c1661864a82d4078843aab83d5e7b102831d9
isaisstillalive <isaisstillalive@gmail.com>
2014-01-08 11:12:07 +0900

    Idolクラスのspecを整理

--- spec/imas_cg/idol_spec.rb
+++ spec/imas_cg/idol_spec.rb
@@ -3,45 +3,69 @@
 require_relative '../spec_helper.rb'
 
 describe ImasCG::Idol do
-  context 'を数値（id）で初期化する場合' do
-    subject(:idol){ ImasCG::Idol.new 3000401 }
+  context 'を数値（id）で初期化した場合' do
+    let(:idol){ described_class.new 3000401 }
 
-    it 'は#idが第一引数である' do
-      expect( idol.id ).to eql 3000401
+    describe '#id' do
+      subject{ idol.id }
+
+      it 'は数値（id）である' do
+        expect( subject ).to eql 3000401
+      end
     end
 
-    it 'は#digestがnilである' do
-      expect( idol.digest ).to eql nil
-    end
-  end
+    describe '#digest' do
+      subject{ idol.digest }
 
-  context 'を文字列（digest）で初期化する場合' do
-    subject(:idol){ ImasCG::Idol.new '6bd01496d9b00da9563c7e92b6a40257' }
-
-    it 'は、digestが第一引数である' do
-      expect( idol.digest ).to eql '6bd01496d9b00da9563c7e92b6a40257'
-    end
-
-    it 'は、idがnilである' do
-      expect( idol.id ).to eql nil
+      it 'はnilである' do
+        expect( subject ).to eql nil
+      end
     end
   end
 
-  context 'をHashで初期化する場合' do
-    subject(:idol){ ImasCG::Idol.new id: 3000401, digest: '6bd01496d9b00da9563c7e92b6a40257' }
+  context 'を文字列（digest）で初期化した場合' do
+    let(:idol){ described_class.new '6bd01496d9b00da9563c7e92b6a40257' }
 
-    it 'は、idまたはdigestのどちらかが指定されていなければエラー' do
-      expect{ ImasCG::Idol.new name: '龍崎薫' }.to raise_error
-      expect{ ImasCG::Idol.new id: 3000401 }.not_to raise_error
-      expect{ ImasCG::Idol.new digest: '6bd01496d9b00da9563c7e92b6a40257' }.not_to raise_error
+    describe '#id' do
+      subject{ idol.id }
+
+      it 'はnilである' do
+        expect( subject ).to eql nil
+      end
     end
 
-    it 'は、idがidキーの値である' do
-      expect( idol.id ).to eql 3000401
+    describe '#digest' do
+      subject{ idol.digest }
+
+      it 'は文字列（digest）である' do
+        expect( subject ).to eql '6bd01496d9b00da9563c7e92b6a40257'
+      end
+    end
+  end
+
+  context 'をHash（データ）で初期化した場合' do
+    let(:idol){ described_class.new id: 3000401, digest: '6bd01496d9b00da9563c7e92b6a40257', name: '龍崎薫' }
+
+    it 'は、idまたはdigestのどちらかが指定されていなければエラーとなる' do
+      expect{ described_class.new name: '龍崎薫' }.to raise_error
+      expect{ described_class.new id: 3000401, name: '龍崎薫' }.not_to raise_error
+      expect{ described_class.new digest: '6bd01496d9b00da9563c7e92b6a40257', name: '龍崎薫' }.not_to raise_error
     end
 
-    it 'は、digestがdigestキーの値である' do
-      expect( idol.digest ).to eql '6bd01496d9b00da9563c7e92b6a40257'
+    describe '#id' do
+      subject{ idol.id }
+
+      it 'はHash[:id]である' do
+        expect( subject ).to eql 3000401
+      end
+    end
+
+    describe '#digest' do
+      subject{ idol.digest }
+
+      it 'はHash[:digest]である' do
+        expect( subject ).to eql '6bd01496d9b00da9563c7e92b6a40257'
+      end
     end
   end
 end
