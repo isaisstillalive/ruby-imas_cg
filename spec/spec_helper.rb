9b2f6879aaa40050801ff18e1bdb97b71a4fdbf3
isaisstillalive <isaisstillalive@gmail.com>
2014-01-08 11:15:32 +0900

    rspecの記述が最新になったので古い記述を使用できないように設定変更

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,12 @@
 
 require_relative '../lib/imas_cg'
 
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
+
 def html path
 	File.read("#{File.dirname(__FILE__)}/html/#{path}", encoding: Encoding::UTF_8)
 end
